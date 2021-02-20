3db8343e2480e2c80ad2ea83eeb73f2d75fd5238
Greg Solovyev <fiddlestring@gmail.com>
2021-02-20 01:17:32 -0800

    Add binance testnet instances

---
+++ src/remote/binancetestnet/HEORewardFarm.js
@@ -0,0 +1,10 @@
+//HEORewardFarm ABI and address
+import web3 from './web3';
+const abi=[{"inputs":[{"internalType":"contract HEOGlobalParameters","name":"globalParams","type":"address"},{"internalType":"contract HEOPriceOracle","name":"priceOracle","type":"address"},{"internalType":"contract IHEOCampaignRegistry","name":"registry","type":"address"}],"stateMutability":"nonpayable","type":"constructor"},{"inputs":[{"internalType":"address","name":"donor","type":"address"},{"internalType":"uint256","name":"amount","type":"uint256"},{"internalType":"address","name":"token","type":"address"}],"name":"addDonation","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"donor","type":"address"}],"name":"getDonationCount","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function","constant":true},{"inputs":[{"internalType":"address","name":"donor","type":"address"},{"internalType":"uint256","name":"di","type":"uint256"}],"name":"claimedReward","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function","constant":true},{"inputs":[{"internalType":"address","name":"destination","type":"address"},{"internalType":"uint256","name":"di","type":"uint256"},{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"claimReward","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"donor","type":"address"},{"internalType":"uint256","name":"di","type":"uint256"}],"name":"calculateReward","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function","constant":true}];
+const address = "0xC580DB0CFe607aa46624Ea9199F499C16D60bbb5";
+const instance = new web3.eth.Contract(
+    abi,
+    address,
+);
+
+export default instance;
