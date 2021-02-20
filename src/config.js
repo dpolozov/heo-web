a7533f25d9d570a68abc2486f6734c9c5d727676
Greg Solovyev <fiddlestring@gmail.com>
2021-02-20 14:06:06 -0800

    Fix Hex addresses

--- src/config.js
+++ src/config.js
@@ -2,13 +2,13 @@ const config = {
     "chainconfigs":{
         "binancetestnet":{
             "currencyOptions":[
-                {"text":"BUSD", "value":"0XED24FC36D5EE211EA25A80239FB8C4CFD80F12EE"},
+                {"text":"BUSD", "value":"0xED24FC36D5EE211EA25A80239FB8C4CFD80F12EE"},
                 {"text":"BNB", "value":"0x0000000000000000000000000000000000000000"}
             ],
             "currencies": {
                 "0x0000000000000000000000000000000000000000":"BNB",
-                "0XED24FC36D5EE211EA25A80239FB8C4CFD80F12EE":"BUSD",
-                "0X337610D27C682E347C9CD60BD4B3B107C9D34DDD":"USDT"
+                "0xED24FC36D5EE211EA25A80239FB8C4CFD80F12EE":"BUSD",
+                "0x337610D27C682E347C9CD60BD4B3B107C9D34DDD":"USDT"
             },
             "contracts":{
                 "HEOToken":"0x6d231B36831c971cFCeBf7263103E2710EbD9B10",
