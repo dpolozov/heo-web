7cda5c1c9eb699f5626d3e9fd4ce70bf10eb7713
Greg Solovyev <fiddlestring@gmail.com>
2021-03-25 11:15:35 -0700

    - added server/ folder for server-side code  - added simple ExpressJS server in server/server.js  - added placeholder routes to server/server.js for server APIs  - routing that is not handled by JS is handled by ReactJS  - server/server.js relies on React build to have placed React files    in /build/, which is what React build normally does  - top level package.json now has "install-all" and "start-reat" scripts.    "npm install-all" runs "npm install" in top level folder for React UI    and in /server/ folder for the server side.  - "npm start" now uses /server/server.js and /server/node_modules for NodeJS server

---
+++ server/server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const path = require('path');
+const AWS = require('aws-sdk');
+const fileUpload = require('express-fileupload');
+const app = express();
+
+// Serve the static files from the React app
+app.use(express.static(path.join(__dirname, '../build')));
+
+//TODO: An api endpoint that will handle image uploads
+app.post('/api/uploadimage', (req,res) => {
+    console.log('Upload image to AWS here');
+});
+
+//TODO: An api endpoint that will handle metadata uploads
+app.post('/api/uploadmeta', (req,res) => {
+    console.log('construct a JSON metadata file and upload it to AWS here');
+});
+
+// Handles any requests that don't match the ones above.
+// All other routing except paths defined above is done by React in the UI
+app.get('*', (req,res) =>{
+    res.sendFile(path.join(__dirname, '..', 'build', 'index.html'));
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port);
+
+console.log('App is listening on port ' + port);
