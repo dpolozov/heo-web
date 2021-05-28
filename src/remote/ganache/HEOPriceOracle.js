a9e4ae66aa1871f25c5ad4de5efb72b7bf0874f0
Greg Solovyev <fiddlestring@gmail.com>
2021-05-28 14:50:28 -0700

    Update ganache contracts

--- src/remote/ganache/HEOPriceOracle.js
+++ src/remote/ganache/HEOPriceOracle.js
@@ -1,7 +1,7 @@
 //HEOPriceOracle ABI and address
 import web3 from './web3';
 const abi=[{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"previousOwner","type":"address"},{"indexed":true,"internalType":"address","name":"newOwner","type":"address"}],"name":"OwnershipTransferred","type":"event"},{"inputs":[],"name":"owner","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function","constant":true},{"inputs":[],"name":"renounceOwnership","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"newOwner","type":"address"}],"name":"transferOwnership","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"token","type":"address"},{"internalType":"uint256","name":"price","type":"uint256"},{"internalType":"uint256","name":"decimals","type":"uint256"}],"name":"setPrice","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"token","type":"address"}],"name":"getPrice","outputs":[{"internalType":"uint256","name":"price","type":"uint256"},{"internalType":"uint256","name":"decimals","type":"uint256"}],"stateMutability":"view","type":"function","constant":true}];
-const address = "0x5496AF2c4af974d5900E5f7AA9842938eC0D1B39";
+const address = "0xA031309f498DAB42bC886cF5D20F69169E5319B3";
 const instance = new web3.eth.Contract(
     abi,
     address,
