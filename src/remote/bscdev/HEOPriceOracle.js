4e86f4f0bb1dea1b93c5227d9e7da26b287ef3fd
Greg Solovyev <fiddlestring@gmail.com>
2021-05-28 16:04:41 -0700

    Update dev net contracts
     - added implementation of HEOCampaign.close
     and HEOCampaign.changeMaxAmount methods

--- src/remote/bscdev/HEOPriceOracle.js
+++ src/remote/bscdev/HEOPriceOracle.js
@@ -1,7 +1,7 @@
 //HEOPriceOracle ABI and address
 import web3 from './web3';
 const abi=[{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"previousOwner","type":"address"},{"indexed":true,"internalType":"address","name":"newOwner","type":"address"}],"name":"OwnershipTransferred","type":"event"},{"inputs":[],"name":"owner","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function","constant":true},{"inputs":[],"name":"renounceOwnership","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"newOwner","type":"address"}],"name":"transferOwnership","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"token","type":"address"},{"internalType":"uint256","name":"price","type":"uint256"},{"internalType":"uint256","name":"decimals","type":"uint256"}],"name":"setPrice","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"token","type":"address"}],"name":"getPrice","outputs":[{"internalType":"uint256","name":"price","type":"uint256"},{"internalType":"uint256","name":"decimals","type":"uint256"}],"stateMutability":"view","type":"function","constant":true}];
-const address = "0xAd67C671B9740184589463069E10DD0f5c528483";
+const address = "0x6D57e51B2Dc18BA92aA684605330b4B8522200A8";
 const instance = new web3.eth.Contract(
     abi,
     address,
