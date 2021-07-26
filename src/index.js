4a60f48a300341e3209a333f8200baa2aa68c9c9
dpolozov <dpolozov@gmail.com>
2021-07-26 16:25:25 -0700

    sentry.io
    Basic setup for sentry.io to track error and performance

--- src/index.js
+++ src/index.js
@@ -7,11 +7,23 @@ import configuration from './config';
 import loadServerProps from './util/serverprops';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './util/i18n';
-
 import './index.css';
-import 'semantic-ui-css/semantic.min.css'
+import 'semantic-ui-css/semantic.min.css';
+import * as Sentry from "@sentry/react";
+import { Integrations } from "@sentry/tracing";
+
+
 
 (function clientJS() {
+    Sentry.init({
+        dsn: "https://bd347d6c25b14066bfe38628a18ca151@o899113.ingest.sentry.io/5880381",
+        integrations: [new Integrations.BrowserTracing()],
+      
+        // Set tracesSampleRate to 1.0 to capture 100%
+        // of transactions for performance monitoring.
+        // We recommend adjusting this value in production
+        tracesSampleRate: 1.0,
+    })
     //Load configs from server
     loadServerProps().then(jsonConfig => {
         for(var a in jsonConfig) {
