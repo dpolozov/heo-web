56f81da6e7da9b8c1e8a09b0dddc7a5d682f4f90
Greg Solovyev <fiddlestring@gmail.com>
2021-12-29 15:01:45 -0800

    Add basic card payment with hard-coded data

--- server/server.js
+++ server/server.js
@@ -374,6 +374,7 @@ APP.post('/api/donatefiat', async (req, res) => {
         if(createCardResp && createCardResp.status >= 200 && createCardResp.data && createCardResp.data.data && createCardResp.data.data.id) {
             let paymentIdempotencyKey = uuidv4();
             //got card ID, can create a payment
+
             let paymentResp = await axios({
                 method: 'post',
                 baseURL: CIRCLE_API_URL,
@@ -490,7 +491,7 @@ APP.get('*', async(req,res) =>{
     var description = "Crowdfunding on blockchain.";
     var image = "https://app.heo.finance/static/media/heo-logo.e772bc1b.png";
     var url = "https://app.heo.finance";
-    var campaign;
+    var campaign, title, description, image;
     var splitURL = req.url.split('/');
     var campaignId = splitURL[splitURL.length -1]
 
